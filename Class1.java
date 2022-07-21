1e7ab0fa74eb74291cd59c4f88c841f5ad1ead4d
robdelar <44275659+robdelar@users.noreply.github.com>
2022-07-21 06:46:36 -0700

    Add files via upload

---
+++ Class1.java
@@ -0,0 +1,72 @@
+package VarBran;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class Class1 {
+	
+	public static void main(String[] args) {
+		int sum, product, largest, smallest;
+		ArrayList<Integer> ints = new ArrayList<Integer>();
+		Scanner myScan = new Scanner(System.in);
+		
+		System.out.println("enter 5 numbers");
+		
+		ints.add(myScan.nextInt());
+		ints.add(myScan.nextInt());
+		ints.add(myScan.nextInt());
+		ints.add(myScan.nextInt());
+		ints.add(myScan.nextInt());
+		myScan.nextLine();
+		
+		sum = 0;
+		for(Integer i : ints) {
+			sum = i + sum;
+		}
+		
+		product = 1;
+		for(Integer i : ints) {
+			product = i * product;
+		}
+		
+		largest = ints.get(0);
+		for(Integer i : ints) {
+			if(i > largest) {
+				largest = i;
+			}
+		}
+		
+		smallest = ints.get(0);
+		for(Integer i : ints) {
+			if(i < smallest) {
+				smallest = i;
+			}
+		}
+		
+		System.out.println("Sum: " + sum + "\nProduct: " + product
+				+ "\nLargest: " + largest + "\nSmallest: " + smallest);
+		
+		HashMap<String, String> cars = new HashMap<>();
+		cars.put("Camry", "Toyota");
+		cars.put("Civic", "Honda");
+		cars.put("Tahoe", "Chevrolet");
+		cars.put("Ultima", "Nissan");
+		
+		String userCar;
+		System.out.println("What car model are you looking for?");
+		userCar = myScan.nextLine();
+		
+		System.out.println("Checking if we have it...");
+		
+		if(cars.get(userCar) != null) {
+			System.out.println("Yes, we have it. This model is part of our " + cars.get(userCar) + " lineup.");
+		}
+		else {
+			System.out.println("We don't have this car.");
+		}
+		
+		
+		
+	}
+
+}
